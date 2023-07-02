dc7b0fa22c6a994746aa3c803f81d0d7eaa8fdbb
BagoesPantera <bagoespantera01@gmail.com>
2023-07-02 14:16:14 +0800

    register and bootswatch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,46 @@
+const { PrismaClient, Prisma } = require("@prisma/client");
+const prisma = new PrismaClient();
 const passport = require("../lib/passport")
+const { generateHash } = require("../lib/bcrypt");
 
 class AuthController {
   static loginPage (req, res) {
     if (req.isAuthenticated()) return res.redirect("/")
 
     res.render("pages/login")
+  }
+
+  static registerPage(req, res) {
+    if (req.isAuthenticated()) return res.redirect("/")
+
+    res.render("pages/register")
+  }
+
+  static async register(req, res, next){
+    if (req.body.password != req.body.confirmPassword) {
+      res.render("pages/register", { messages:{error:"Password and Confirm Password doesnt match!"} });
+    }else{
+      try {
+        await prisma.user.create({
+          data: {
+            username: req.body.username,
+            password: await generateHash(req.body.password)
+          }
+        })
+        passport.authenticate("local", {
+          successRedirect: "/",
+          failureRedirect: "/login",
+          failureFlash: true, // Untuk mengaktifkan express flash
+        })(req, res, next);
+      } catch (err) {
+        if (err instanceof Prisma.PrismaClientKnownRequestError) {
+          if (err.code === "P2002") {
+            req.flash("error", "An username with this name is already in use");
+          }
+        }
+        res.redirect("/register")
+      }
+    } 
   }
 
   static async login(req, res, next) {
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -6,6 +6,8 @@ const router = Router();
 
 router.get("/login", AuthController.loginPage)
 router.post("/login", AuthController.login)
+router.get("/register", AuthController.registerPage)
+router.post("/register", AuthController.register)
 router.post("/logout", AuthController.logout)
 
 module.exports = router; 
